90d1ee14d5e63ad8e8ee79f1fc9a693145f6643e
Jonathan Cobos <jonathan.cobos@outlook.es>
2023-08-14 21:50:21 -0600

    Added the login page, styled and added the header

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import { webfontDownload } from "vite-plugin-webfont-dl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),],
+  plugins: [react(), webfontDownload()],
   server: {
     port: 3000,
     open: true,
