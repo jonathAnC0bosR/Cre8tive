7d86ef066c12c56c7cc38e1708319f187df97f74
Lizzz <alejandroraidi@gmail.com>
2023-08-15 18:57:57 -0600

    resolvers sketch

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,10 +5,35 @@ const typeDefs = `
         postContent: String
         createdAt: String
         comments: String
+        userID(_id: ID!):User
+    }
+
+    type Bulletin{
+        _id: ID
+        bulletPostTitle: String
+        offert: String
+        expectation: String
+        createdAt: String
+        userID(_id: ID!):User
+    }
+
+    type User{
+        _id: ID
+        username: String
+        mail: String
+        password: String
+        age: Int
+        occupation: String
+        location: String
+        rating: Int
+        bulletinPosts(_id: ID!): Bulletin
+        portfolioPosts(_id: ID!): Portfolio
     }
 
     type Query {
         portfolioPosts: [Portfolio]
+        bulletinPosts: [Bulletin]
+        user: User
     }
 `;
 
