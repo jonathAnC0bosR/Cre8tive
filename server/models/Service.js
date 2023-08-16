bb58ca4ab0952d28e6c15f1ab03af5b116a12b4f
Lizzz <alejandroraidi@gmail.com>
2023-08-15 20:43:29 -0600

    skill service update bulletin

---
+++ server/models/Service.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const serviceSchema = new Schema({
+    serviceTitle:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    serviceDescription:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    userID:{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    skillID:{
+        type: Schema.Types.ObjectId,
+        ref: 'Skill'
+    },
+});
+
+const Service = mongoose.model('Service', serviceSchema);
+
+module.exports = { serviceSchema };
---
+++ server/models/Skill.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const skillSchema = new Schema({
+    skillTitle:{
+        type: String,
+        required: true,
+    },
+    userID:{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+const Skill = mongoose.model('Skill', skillSchema );
+
+module.exports = Skill;
