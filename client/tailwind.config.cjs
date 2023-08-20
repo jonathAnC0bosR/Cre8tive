ab16f7a00eda0096cd2649a41e1d4854e846e901
LeShush <mycelium.architecture@gmail.com>
2023-08-20 14:43:30 -0600

    Added spinner functionality to button and changes to the Services modal

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -3,7 +3,6 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js",
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
