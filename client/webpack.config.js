d0c940882a50d54cab530e3cda7bfa27ae8d56f6
LeShush <mycelium.architecture@gmail.com>
2023-08-21 13:04:59 -0600

    Tested some TypeScript addons for extra functionality, no t worked

---
+++ client/webpack.config.js
@@ -0,0 +1,37 @@
+//webpack.config.js
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: {
+    main: "./src/main.jsx",
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: "app-bundle.js" // <--- Will be compiled to this single file
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.tsx?$/,
+        loader: "ts-loader"
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        // We use babel-loader in order to use ES6.
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+          },
+        },
+      },
+    ]
+  }
+};
