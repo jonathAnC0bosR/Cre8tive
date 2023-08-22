a80513c15d81255901af8263df6029b03cd1ff8c
LeShush <mycelium.architecture@gmail.com>
2023-08-22 07:08:27 -0600

    Changes to Profile Page

--- client/src/components/Pages/Profile/Profile3.jsx
+++ client/src/components/Pages/Profile/Profile3.jsx
@@ -121,158 +121,149 @@ const Profile = () => {
 
 
     return (
-        <div id="profile div" className='pt-[70px] bg-gradient-to-r from-[#0C0F11] to-[#22282D]'>
-            <div className='mainContainer' >
-                <div className='topBaner'>
+        <>
+            <div className="lg:h-16 h-10 md:h-16 bg-gradient-to-r from-[#0C0F11] to-[#22282D]"></div> {/* Spacer */}
+
+            <div id="profile div" className='lg:text-sm min-h-screen bg-gradient-to-r from-[#0C0F11] to-[#22282D] flex items-center justify-center  '>
+                <div className='topBaner fixed align-top top-0 min-w-full min-h-10 h-36'>
                     <img src={topBanerImg2}></img>
                 </div>
-                <div className='profileContainer flex items-center justify-center '>
-                    {/* -------------------------profile image and icon */}
-                    <div className='flex items-center justify-center' >
-                        <div className='flex relative  ' >
-                            <div className="overflow-hidden rounded-full w-32 h-32">
-                                <img
-                                    src={image}
-                                    alt="Circular Image"
-                                    className="w-full h-full object-cover"
+                <div className='mainContainer xl:w-1/3 lg:w-1/2 md:w-1/2 w-5/6 mt-24' >
+
+                    <div className='profileContainer flex items-center justify-center '>
+                        {/* -------------------------profile image and icon */}
+                        <div className='flex items-center justify-center' >
+                            <div className='flex relative  ' >
+                                <div className="overflow-hidden rounded-full xl:w-64 xl:h-64 lg:w-48 lg:h-48">
+                                    <img
+                                        src={image}
+                                        alt="Circular Image"
+                                        className="w-full h-full object-cover"
+                                    />
+
+                                </div>
+                                <div className='absolute bottom-1 left-0'>
+                                    <button onClick={handleEditProfile} className='bg-pink8 text-white font-bold rounded-full p-2' >Edit Profile
+                                    </button>
+                                </div>
+                                <div className="absolute bottom-0 right-0  " >
+                                    <UploadPencil updateProp={updateStateURL} />
+                                </div>
+
+                            </div>
+                        </div>
+
+
+                        <div className='profileInfo flex flex-col items-center justify-center mt-6'>
+                            <h3 className="text-5xl font-extrabold"> {userData.username} </h3>
+                            <p className='mt-2 mb-6 text-pink8 font-bold'> {userData.occupation} </p>
+
+                            {/* <a onClick={openEditProfileModal}> Edit profile</a> */}
+
+
+                            <div className="profileInfo flex flex-col items-center justify-center my-5" >
+                                <h3 className="text-white text-xl my-4 font-extrabold"> About me</h3>
+                                <p className="font-thin text-center"> {userData.aboutMe} </p>
+                            </div>
+
+                            <div className='flex items-center justify-center m-5 w-1/2 p-4
+                         grid-cols-4 gap-4 '>
+                                <div className='flex flex-row row-auto justify-between' >
+                                    <a href="mailto:your.email@example.com" className='flex-end' >
+                                        <HiOutlineMail size={"3em"} />
+                                    </a>
+                                    <a>
+                                        <img src={artStation} className='w-10 mx-4' ></img>
+                                    </a>
+                                    <button>
+                                        <BsGithub size={"2em"} color='white' />
+                                    </button>
+                                </div>
+
+
+
+
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col items-center w-1/2 rounded-xl p-4 mb-6">
+
+                            <h3 className="text-white text-xl my-4 font-extrabold" >Skills</h3>
+                            <div className='flex items-center justify-center'>
+
+                                <div className='bulletSkill'>
+                                    <BsFillCameraFill />
+                                    <p>Skill One</p>
+                                </div>
+                                <div className='bulletSkill'>
+                                    <BsFillCameraFill />
+                                    <p>Skill One</p>
+                                </div>
+                                <div className='bulletSkill'>
+                                    <BsFillCameraFill />
+                                    <p>Skill two</p>
+                                </div>
+                                <div className='bulletSkill'>
+                                    <BsFillCameraFill />
+                                    <p>Skill two</p>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div className='portfolioContainer items-center flex flex-col justify-center grid-cols-4 mt-6'>
+                            <h3 className="text-white text-xl my-4 font-extrabold"> Portfolio</h3>
+                            <div className='firtsRowMedia col-span-4' style={{ display: 'flex', justifyContent: 'center' }}>
+                                <PinterestEmbed url="https://www.pinterest.com.mx/pin/374995106486676585/"
+                                    width={200}
+                                    height={200}
                                 />
-
+                                <PinterestEmbed url="https://www.pinterest.com.mx/pin/374995106486676581/"
+                                    width={200}
+                                    height={200}
+                                />
+                                <PinterestEmbed url="https://www.pinterest.com.mx/pin/374995106486676570/"
+                                    width={200}
+                                    height={200}
+                                />
+                                <PinterestEmbed url="https://www.pinterest.com.mx/pin/374995106486676558/"
+                                    width={200}
+                                    height={200}
+                                />
                             </div>
-
-                            <div className="absolute top-0 right-0  " >
-                                <UploadPencil updateProp={updateStateURL} />
-                            </div>
-
-                        </div>
-                    </div>
-
-                    <div className='profileInfo flex flex-col items-center justify-center my-5'>
-                        <h1  > {username} </h1>
-                        <p className='mb-7 text-orange-500 font-bold'> {userData.occupation} </p>
-                        <button onClick={handleEditProfile} className='bg-white text-orange-500 font-bold rounded-full py-1 px-2 text-sm' >Edit Profile </button>
-                        {/* <a onClick={openEditProfileModal}> Edit profile</a> */}
-
-
-                        <div className="profileInfo flex flex-col items-center justify-center my-5" >
-                            <h2 > About me</h2>
-                            <p> {userData.aboutMe} </p>
-                        </div>
-
-                        <div className='flex items-center justify-center m-5 w-1/2 p-4
-                        grid grid-cols-4 gap-4 '>
-                            <div className='flex items-center justify-center' >
-                                <a href="mailto:your.email@example.com" className='flex-end' >
-                                    <HiOutlineMail size={"2em"} />
-                                </a>
-                            </div>
-                            {/* <div className="col-span-3" >
-                                    <a>
-                                        <img src={artStation} className='w-10' ></img>
-                                    </a>
-                                    <button> 
-                                        <BsGithub size={"2em"} color='white'/>
-                                    </button>
-                            </div> */}
-                            {userData.artStation && userData.github ?
-                                <div className="col-span-3" >
-                                    <a>
-                                        <img src={artStation} className='w-10' ></img>
-                                    </a>
-                                    <button>
-                                        <BsGithub size={"2em"} color='white' />
-                                    </button>
-                                </div>
-                                :
-                                <div className="col-span-3" >
-                                    <a>
-                                        <img src={artStation} className='w-10' ></img>
-                                    </a>
-                                    <button>
-                                        <BsGithub size={"2em"} color='white' />
-                                    </button>
-                                </div>
-                            }
-
-
-                        </div>
-                    </div>
-
-                    <div className="flex flex-col items-center bg-slate-800 w-1/2 rounded-xl p-4">
-
-                        <h3 className="text-white text-xl" >Skills</h3>
-                        <div className='flex items-center justify-center'>
-
-                            <div className='bulletSkill'>
-                                <BsFillCameraFill />
-                                <p>Skill One</p>
-                            </div>
-                            <div className='bulletSkill'>
-                                <BsFillCameraFill />
-                                <p>Skill One</p>
-                            </div>
-                            <div className='bulletSkill'>
-                                <BsFillCameraFill />
-                                <p>Skill two</p>
-                            </div>
-                            <div className='bulletSkill'>
-                                <BsFillCameraFill />
-                                <p>Skill two</p>
+                            <div className='secondsRowMedia col-span-4' style={{ display: 'flex', justifyContent: 'center' }}>
+                                <PinterestEmbed url="https://www.pinterest.com.mx/pin/374995106486676547/"
+                                    width={200}
+                                    height={200}
+                                />
+                                <PinterestEmbed url="https://www.pinterest.com.mx/pin/374995106486676535/"
+                                    width={200}
+                                    height={200}
+                                />
+                                <PinterestEmbed url="https://www.pinterest.com.mx/pin/68609594315630760/"
+                                    width={200}
+                                    height={200}
+                                />
+                                <PinterestEmbed url="https://www.pinterest.com.mx/pin/374995106486676552/"
+                                    width={200}
+                                    height={200}
+                                />
                             </div>
                         </div>
+                        {/* <Posts/> To be added*/}
 
                     </div>
-
-                    <div className='portfolioContainer'>
-                        <h3>Portfolio</h3>
-                        <div className='firtsRowMedia' style={{ display: 'flex', justifyContent: 'center' }}>
-                            <PinterestEmbed url="https://www.pinterest.com.mx/pin/9570217950888311/"
-                                width={200}
-                                height={200}
-                            />
-                            <PinterestEmbed url="https://www.pinterest.com.mx/pin/670895675752727009/"
-                                width={200}
-                                height={200}
-                            />
-                            <PinterestEmbed url="https://www.pinterest.com.mx/pin/612067405622271768/"
-                                width={200}
-                                height={200}
-                            />
-                            <PinterestEmbed url="https://www.pinterest.com.mx/pin/654007177161332170/"
-                                width={200}
-                                height={200}
-                            />
-                        </div>
-                        <div className='secondsRowMedia' style={{ display: 'flex', justifyContent: 'center' }}>
-                            <PinterestEmbed url="https://www.pinterest.com.mx/pin/55732114131342304/"
-                                width={200}
-                                height={200}
-                            />
-                            <PinterestEmbed url="https://www.pinterest.com.mx/pin/55732114131342304/"
-                                width={200}
-                                height={200}
-                            />
-                            <PinterestEmbed url="https://www.pinterest.com.mx/pin/55732114131342304/"
-                                width={200}
-                                height={200}
-                            />
-                            <PinterestEmbed url="https://www.pinterest.com.mx/pin/55732114131342304/"
-                                width={200}
-                                height={200}
-                            />
-                        </div>
-                    </div>
-                    {/* <Posts/> To be added*/}
-
                 </div>
+                <br></br>
+                <br></br>
+                <br></br>
+                {isEditProfileModalOpen && (
+                    <EditProfile onClose={closeEditProfileModal} />
+                )}
             </div>
-            <br></br>
-            <br></br>
-            <br></br>
-            {isEditProfileModalOpen && (
-                <EditProfile onClose={closeEditProfileModal} />
-            )}
-        </div>
+        </>
     )
+
 }
 
 export default Profile;
