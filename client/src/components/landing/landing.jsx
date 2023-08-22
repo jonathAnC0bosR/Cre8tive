fff8d7ccd8581843bfdc6a4440df13f694c70c69
HectorHJDH <hectorjosue78@gmail.com>
2023-08-22 17:53:37 -0600

    Minor animations to landing page

--- client/src/components/landing/landing.jsx
+++ client/src/components/landing/landing.jsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import backimg from "../../assets/images/Cre8ive_Bckground-02.png";
 import { Link } from "react-router-dom";
+import "../../index.css";
 
 const Landing = () => {
   const Variants = {
@@ -23,23 +24,23 @@ const Landing = () => {
       <div className="grid grid-cols-1 lg:flex lg:flex-row lg:min-h-screen mb-5">
         <div className="lg:basis-1/3 text-center lg:p-28 p-0 z-[2]  ">
           <div className="mb-16">
-            <h1 className="text-white text-2xl md:text-5xl lg:text-7xl">
+            <h1 className="text-white text-2xl md:text-5xl lg:text-7xl fade-in">
               Let's get
             </h1>
-            <div className="flex justify-center text-4xl md:text-7xl lg:text-9xl ">
-              <h1 className="text-white   ">Cre</h1>
-              <h1 className="text-orange-500 ">8</h1>
-              <h1 className="text-white ">tive</h1>
+            <div className="flex justify-center text-4xl md:text-7xl lg:text-9xl fade-in">
+              <h1 className="text-white">Cre</h1>
+              <h1 className="text-orange-500">8</h1>
+              <h1 className="text-white">tive</h1>
             </div>
           </div>
-          <p className="text-white  md:text-xl lg:text-2xl">
+          <p className="text-white  md:text-xl lg:text-2xl fade-in-txt">
             Connect, Collaborate, and Trade with like-minded individuals in our
             community-driven platform!
           </p>
         </div>
         <img
           src={backimg}
-          className="lg:w-2/3 mx-auto lg:absolute lg:right-0 lg:top-0 z-[1] "
+          className="lg:w-2/3 mx-auto lg:absolute lg:right-0 lg:top-0 z-[1] fade-in"
         ></img>
       </div>
 
@@ -54,7 +55,10 @@ const Landing = () => {
             to exchange talents, expertise, and assistance.
           </p>
           <p className="p-[2%]">
-          Discover the simplicity of our platform: just post the job you need assistance with from an artist, and in return, list the skill or service you can offer.          </p>
+            Discover the simplicity of our platform: just post the job you need
+            assistance with from an artist, and in return, list the skill or
+            service you can offer.{" "}
+          </p>
         </div>
       </div>
       <div className="flex bg-zinc-950 mb-[2.5%] p-6 w-4/5 rounded-lg text-white  grid grid-cols-2 shadow-xl shadow-stone-950">
@@ -63,15 +67,18 @@ const Landing = () => {
             Exchange Freely
           </h1>
           <p className="p-[2%] w-3/4 ">
-          Benefit from free interactions, elevate your projects, and embrace the freedom of job interchange.          </p>
+            Benefit from free interactions, elevate your projects, and embrace
+            the freedom of job interchange.{" "}
+          </p>
         </div>
         <div className="">
           <h1 className="text-xl lg:text-center md:text-1xl lg:text-2xl m-[1%]">
             New Economy
           </h1>
           <p className="p-[2%]">
-          Unlocking artistry and fostering a collaborative economy, our platform unites individuals to share skills, services, and expertise within the vibrant community.
-
+            Unlocking artistry and fostering a collaborative economy, our
+            platform unites individuals to share skills, services, and expertise
+            within the vibrant community.
           </p>
         </div>
       </div>
@@ -80,7 +87,7 @@ const Landing = () => {
         <h1 className=" mb-[12%] text-white text-xl md:text-2xl lg:text-4xl">
           Join now!
         </h1>
-        <Link to={'/signup'}>
+        <Link to={"/signup"}>
           <motion.button
             variants={Variants}
             whileHover={{ scale: 1.1 }}
